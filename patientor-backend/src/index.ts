b92c5bc430cb5eb87e63e043e51b8d12ee0a0fe9
Dvwhite81 <Dvwhite1981@gmail.com>
2023-11-02 13:18:06 -0400

    9.11 - complete

--- patientor-backend/src/index.ts
+++ patientor-backend/src/index.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import cors = require('cors');
-
+import diagnosisRouter from './routes/diagnosesRoutes';
+import patientRouter from './routes/patientRoutes';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const PORT = 3001;
+
+app.use('/api/diagnoses', diagnosisRouter);
+app.use('/api/patients', patientRouter);
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
